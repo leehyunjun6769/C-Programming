c1d9239bc39b9eb969e1a8f4ff8c9051c1b6cc63
leehyunjun6769 <128291239+leehyunjun6769@users.noreply.github.com>
2023-04-09 20:07:45 +0900

    While문 4.6 목요일 수업

---
+++ C언어/While문/while2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n, sum = 0;
+	int i = 1;
+
+	printf("정수를 입력하시오 : ");
+	scanf("%d", &n);
+
+	while (i <= n) 
+	{
+		sum += i;
+		i++;
+	}
+	printf("1부터 %d까지의 합은 %d입니다.", n, sum);
+	return 0;
+}
---
+++ C언어/While문/while3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n, sum = 0;
+	int i = 1;
+
+	printf("정수를 입력하시오 : ");
+	scanf("%d", &n);
+
+	while (i <= n)
+	{
+		if (i % 2 == 0)
+		{
+			sum += i;
+		}
+		i++;
+	}
+	printf("1부터 %d까지의 짝수합은 %d입니다.", n, sum);
+	return 0;
+}
