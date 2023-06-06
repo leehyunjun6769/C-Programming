5307277f1b1290a3669abc50831694037890cbc4
leehyunjun6769 <guswns6769@naver.com>
2023-06-06 13:25:20 +0900

    Create happybirFunction.c

---
+++ 6.6 scanf로 줄바꿈시도/happybirFunction.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void happyBirthday();
+
+int main()
+{
+	happyBirthday();
+	return 0;
+}
+
+void happyBirthday()
+{
+
+	char a[100] = "";
+
+	scanf("%s", &a);
+	printf("%s\n", a);
+}
